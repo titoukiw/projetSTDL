9c60dcb7176b62ebc53871b4184261f0dd388e9c
Rit00n <henroy@hotmail.fr>
2017-05-10 14:51:57 +0200

    rm InterfaceFactory

--- ProjetSTDL/fr/n7/stl/block/ast/impl/InterfaceImpl.java
+++ ProjetSTDL/fr/n7/stl/block/ast/impl/InterfaceImpl.java
@@ -1,15 +1,21 @@
-package fr.n7.stl.interface.ast.impl;
+package fr.n7.stl.block.ast.impl;
 
 /**
  * @author okeee
  *
  */
 
-import fr.n7.stl.interface.ast.interface;
+import fr.n7.stl.block.ast.interface;
 
 public class InterfaceImpl implements Interface {
 
 		private String nom;
+		private 
+
+
+		public InterfaceImpl(String nom, LinkedList<ElementInterface> elements){
+			this.nom = nom;
+		}
 
 		public InterfaceImpl(String nom, LinkedList<ParametreGenericite> parametresGen, LinkedList<Type> heritages, LinkedList<ElementInterface> elements){
 			this.nom = nom;
@@ -23,9 +29,7 @@ public class InterfaceImpl implements Interface {
 			this.nom = nom;
 		}
 
-		public InterfaceImpl(String nom, LinkedList<ElementInterface> elements){
-			this.nom = nom;
-		}
+
 
 		public String getName(){
 			return this.nom;
