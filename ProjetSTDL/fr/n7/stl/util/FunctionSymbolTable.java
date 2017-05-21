6aadc19fe1a050085fe6a273b80ed21bf8ccbce9
titouan <titouan.tanguy@gmail.com>
2017-05-21 18:12:05 +0200

    ajout ast classes

--- ProjetSTDL/fr/n7/stl/util/FunctionSymbolTable.java
+++ ProjetSTDL/fr/n7/stl/util/FunctionSymbolTable.java
@@ -7,25 +7,42 @@ package fr.n7.stl.util;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-
 import fr.n7.stl.block.ast.*;
 import fr.n7.stl.block.ast.impl.*;
 
 
 public class FunctionSymbolTable{
 	private LinkedList<Signature> listSignatures;
-	private LinkedList<Objet> listMethodes;
-	private LinkedList<Objet> listConstructeurs;
+	private LinkedList<Methode> listMethodes;
+	private LinkedList<Constructeur> listConstructeurs;
 
 
 	public FunctionSymbolTable() {
 		this.listSignatures = new LinkedList<Signature>();
-		this.listMethodes = new LinkedList<Objet>();
-		this.listConstructeurs = new LinkedList<Objet>();
+		this.listMethodes = new LinkedList<Methode>();
+		this.listConstructeurs = new LinkedList<Constructeur>();
 	}
 
 	/* To complete */
 
+
+
+	public boolean containCons(Constructeur cons) {
+		for (Constructeur c_element : this.listConstructeurs) {
+			if(c_element.isEqual(cons)){
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean containFun(Methode met) {
+		for(Methode m_element : this.listMethodes){
+			if(m_element.isEqual(met)){
+				return true;
+			}
+		}
+		return false;
+	}
 	public boolean containSign(Signature sign){
 		for(Signature s_element : this.listSignatures){
 			if(s_element.isEqual(sign)) {
@@ -35,11 +52,36 @@ public class FunctionSymbolTable{
 		return false;
 	}
 
+
+
+	public boolean acceptConst(Constructeur cons) {
+		return (!this.containCons(cons));
+	}
+	public boolean acceptFun(Methode met) {
+		return (!this.containFun(met));
+	}
+
 	public boolean acceptSign(Signature sign){
 		return (!this.containSign(sign));
 	}
 
 
+
+	public void registerConstructeur(Constructeur cons) throws ForbiddenDeclarationException {
+		if(this.acceptConst(cons)) {
+			this.listConstructeurs.add(cons);
+		} else {
+			throw new ForbiddenDeclarationException();
+		}
+	}
+
+	public void registerFun(Methode met) throws ForbiddenDeclarationException {
+		if(this.acceptFun(met)) {
+			this.listMethodes.add(met);
+		} else {
+			throw new ForbiddenDeclarationException();
+		}
+	}
 
 	public void registerSign(Signature sign, String interfaceCourrante) throws ForbiddenDeclarationException {
 		if(this.acceptSign(sign)){
