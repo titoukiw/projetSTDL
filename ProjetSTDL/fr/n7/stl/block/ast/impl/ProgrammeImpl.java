ec4262499c7f9570d1471f9bf7f5882ead0681f0
titouan <titouan.tanguy@gmail.com>
2017-05-21 18:30:28 +0200

    changement ProgrammeImpl.java

--- ProjetSTDL/fr/n7/stl/block/ast/impl/ProgrammeImpl.java
+++ ProjetSTDL/fr/n7/stl/block/ast/impl/ProgrammeImpl.java
@@ -7,15 +7,20 @@ import fr.n7.stl.block.ast.*;
 public class ProgrammeImpl implements Programme {
 
 	private LinkedList<Interface> interfaces;
+	private LinkedList<Classe> classes
 
-	public ProgrammeImpl(LinkedList<Interface> interfaces) {
+	public ProgrammeImpl(LinkedList<Interface> interfaces, LinkedList<Classe> classes) {
 		this.interfaces = interfaces;
+		this.classes = classes;
 	}
 
 	public String toString() {
 		String result = "";
 		for(Objet obj:interfaces){
 			result += obj.toString();
+		}
+		for(Classe classe : this.classes) {
+			result += classe.toString)();
 		}
 		return result;
 	}
