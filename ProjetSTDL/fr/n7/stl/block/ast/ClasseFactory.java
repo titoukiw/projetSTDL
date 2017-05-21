0b0bbdb263ea4f6d55abb2730c738183d0f9fd69
Rit00n <henroy@hotmail.fr>
2017-05-21 18:59:39 +0200

    ClasseFactory.java update

---
+++ ProjetSTDL/fr/n7/stl/block/ast/ClasseFactory.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+
+package fr.n7.stl.block.ast;
+import java.util.LinkedList;
+
+
+public interface ClasseFactory {
+
+	/* createClasse */
+	public Classe createClasse(String nom, String classeCourante, LinkedList<Classe> heritages, LinkedList<Interface> impl,LinkedList<ElementClasse> elements);
+
+
+	/* Genericite*/
+
+	/* Elements Classe*/
+	public Constructeur createConstructeur(String id, String classeCourante, DroitAcces droit, LinkedList<Parametre> listParam, Block body);
+	public Methode createMethode(String id, String classeCourante, DroitAcces droit, 
+							     LinkedList<Parametre> listParam, Bloc body, boolean statique, Type returnType);
+	public Attribut createAttribut(String id, String classeCourante, Type type, DroitAcces droit, boolean statique);
+
+
+}
