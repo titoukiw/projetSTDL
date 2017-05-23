fa3037a8b22c417f185463259542d4580f99e18b
Rit00n <henroy@hotmail.fr>
2017-05-23 16:49:15 +0200

    nv test_default

--- ProjetSTDL/nbRegleManq.sh
+++ ProjetSTDL/nbRegleManq.sh
@@ -6,6 +6,9 @@ grep '\->' testMiniJava.egg | wc -l
 echo dont 
 grep '\->' testMiniJava.egg | grep '^\-\-' | wc -l
 echo sont commentées
+echo ——————————————————————
+echo nb lignes avec 'eneri' et '->'
+grep eneri testMiniJava.egg | grep '\->' | wc -l
 
 echo ——————————————————————
 echo MJ
@@ -15,3 +18,6 @@ grep '\->' MiniJava.egg | wc -l
 echo dont 
 grep '\->' MiniJava.egg | grep '^\-\-' | wc -l
 echo sont commentées
+echo ——————————————————————
+echo nb lignes avec 'eneri' et '->'
+grep eneri testMiniJava.egg | grep '\->' | wc -l
