8b092fa16b9a89d961047fe7a799bccc6405ccb2
titouan <titouan.tanguy@gmail.com>
2017-05-24 22:35:09 +0200

    UndeclaredTypeImpl.java

---
+++ ProjetSTDL/fr/n7/stl/block/ast/impl/UndeclaredTypeImpl.java
@@ -0,0 +1,37 @@
+package fr.n7.stl.block.ast.impl;
+
+import java.util.LinkedList;
+
+import fr.n7.stl.block.ast.*;
+
+
+public class UndeclaredTypeImpl implements Type {
+
+	private String nom;
+
+	public UndeclaredTypeImpl(String nom){
+		this.nom = nom;
+	}
+
+	public String getName(){
+		return this.nom;
+	}
+
+	public equalsTo(Type _other){
+		throw new SemanticsUndefinedException("equalsTo no meaning for UndeclaredTypeImpl");
+	}
+
+	public boolean compatibleWith(Type _other){
+		throw new SemanticsUndefinedException("equalsTo no meaning for UndeclaredTypeImpl");
+	}
+	
+	
+	public Type merge(Type _other){
+		throw new SemanticsUndefinedException("merge no meaning for UndeclaredTypeImpl");
+	}
+	
+
+	public int length(){
+		throw new SemanticsUndefinedException("length no meaning for UndeclaredTypeImpl");
+	}
+}
